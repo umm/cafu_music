8b5ee475454c5bf4c703bd8736442e67dd4e6d5d
Tetsuya Mori <monry84@gmail.com>
2018-02-13 00:29:30 +0900

    :sparkles: Implement Installer

---
+++ Assets/Scripts/CAFU/Music/MusicInstaller.cs
@@ -0,0 +1,20 @@
+﻿using CAFU.Music.Domain.Model;
+using CAFU.Music.Domain.Repository;
+using CAFU.Music.Domain.UseCase;
+using Zenject;
+
+namespace Modules.Scripts.CAFU.Music {
+
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MusicInstaller<TEnum> : Installer<MusicInstaller<TEnum>>
+        where TEnum : struct {
+
+        public override void InstallBindings() {
+            this.Container.Bind<IMusicUseCase<TEnum>>().To<MusicUseCase<TEnum>>().AsTransient();
+            this.Container.Bind<IMusicRepository<TEnum>>().To<MusicRepository<TEnum>>().AsTransient();
+            this.Container.Bind<IMusicModel>().To<MusicModel>().AsTransient();
+        }
+
+    }
+
+}
