9c79cac27f7afbd6916541df45730beea91ef28a
Tetsuya Mori <monry84@gmail.com>
2018-02-13 15:03:07 +0900

    :truck: Move old script into BackwardCompatibility

---
+++ Assets/Scripts/CAFU/Music/BackwardCompatibility/Presentation/Presenter/MusicPresenter.cs
@@ -0,0 +1,12 @@
+﻿
+
+// ReSharper disable UnusedMember.Global
+
+namespace Assets.Scripts.CAFU.Music.Presentation.Presenter {
+
+    public interface IMusicPresenter<in TEnum> : global::CAFU.Music.Presentation.Presenter.IMusicPresenter<TEnum>
+        where TEnum : struct {
+
+    }
+
+}
