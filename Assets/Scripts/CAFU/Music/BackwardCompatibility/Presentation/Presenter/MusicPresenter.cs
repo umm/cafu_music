5b4873a71c97953aad795bba5b6047c68ad53910
Tetsuya Mori <monry84@gmail.com>
2018-02-13 15:04:35 +0900

    :memo: Add Obsolete

--- Assets/Scripts/CAFU/Music/BackwardCompatibility/Presentation/Presenter/MusicPresenter.cs
+++ Assets/Scripts/CAFU/Music/BackwardCompatibility/Presentation/Presenter/MusicPresenter.cs
@@ -2,8 +2,11 @@
 
 // ReSharper disable UnusedMember.Global
 
+using System;
+
 namespace Assets.Scripts.CAFU.Music.Presentation.Presenter {
 
+    [Obsolete("Please use 'CAFU.Music.Presentation.Presenter.IMusicPresenter<TEnum>' instead of this interface.")]
     public interface IMusicPresenter<in TEnum> : global::CAFU.Music.Presentation.Presenter.IMusicPresenter<TEnum>
         where TEnum : struct {
 
