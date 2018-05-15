c5b1022d2be109c762fdf8c8e292bc29d0198b35
Tetsuya Mori <monry84@gmail.com>
2018-05-15 16:47:12 +0900

    :sparkles: Add generator for MusicEntity

---
+++ Assets/Editor/Generator/MusicEntity.cs
@@ -0,0 +1,44 @@
+﻿using CAFU.Generator;
+using CAFU.Generator.Enumerates;
+using JetBrains.Annotations;
+
+namespace CAFU.Music.Generator
+{
+    [UsedImplicitly]
+    public class MusicEntity : ClassStructureBase
+    {
+        private const string StructureName = "Data/Entities/MusicEntity";
+
+        public override string Name { get; } = StructureName;
+
+        protected override ParentLayerType ParentLayerType { get; } = ParentLayerType.Data;
+
+        protected override LayerType LayerType { get; } = LayerType.Entity;
+
+        protected override string ModuleName { get; } = "umm@cafu_music";
+
+        public override void Generate(bool overwrite)
+        {
+            var parameter = new Parameter()
+            {
+                ParentLayerType = ParentLayerType,
+                LayerType = LayerType,
+                ClassName = "MusicEntity",
+                Overwrite = overwrite,
+            };
+            parameter.Namespace = CreateNamespace(parameter);
+
+            parameter.UsingList.Add("System");
+            parameter.UsingList.Add("CAFU.Generics.Data.Entity");
+            parameter.UsingList.Add("CAFU.Music.Data.Entity");
+            parameter.UsingList.Add($"{GeneratorWindow.ProjectContext.NamespacePrefix.Trim('.')}.Application.Enumerates");
+            parameter.UsingList.Add("UnityEngine");
+            parameter.BaseClassName = "ScriptableObjectGenericEntity";
+            parameter.ImplementsInterfaceList.Add("IMusicEntity<MusicName>");
+
+            var generator = new ScriptGenerator(parameter, CreateTemplatePath(TemplateType.Class, StructureName));
+
+            generator.Generate(CreateOutputPath(parameter));
+        }
+    }
+}
